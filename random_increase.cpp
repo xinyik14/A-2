7c67e74db49919f1d4928c7d14d8cd69b6009f3c
Kathy Zhang <xinyik14@yahoo.com>
2015-02-12 11:45:05 -0600

    added input file with arrays of increasing order and decreasing order.

--- random_increase.cpp
+++ random_increase.cpp
@@ -8,14 +8,7 @@
 using namespace std;
 
 
-void generate_random(int* A, int n){
 
-	srand(time(0));//initialize random seed
-	for(int i = 0; i < n; i++){
-		A[i] = rand();
-	}
-	
-}
 
 void write_infile(ofstream& writer, int* Arr, int n){
 	for(int i = 0; i < n; i++){
@@ -27,12 +20,15 @@ void write_infile(ofstream& writer, int* Arr, int n){
 
 
 int main(){
-	ofstream writer("input_random.txt", ofstream::app);
+	ofstream writer("input_increase.txt", ofstream::app);
 	int n = pow(10, 2);
 	writer<<n<<endl;
 	int* Arr = new int[n];
-	generate_random(Arr, n);
-	//add sort
+
+	for(int i = 0; i < n; i++){
+		Arr[i] = i;
+	}
+
 	write_infile(writer, Arr, n);
 	writer.close();
 }
