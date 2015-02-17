92198ef23dc14b47674f14fe3fe5f0a0a5685ec2
Kathy Zhang <xinyik14@yahoo.com>
2015-02-17 11:52:51 -0600

    Changed selection sort

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -13,14 +13,15 @@
 void
 SelectionSort::sort(int A[], int size)				// main entry point
 {
-	for(int k = 0; k < size-1; k++){
-		int min = A[k];
-		for(int i = k+1; i < size && (num_cmps++,A[i] < min); i++){
-			int temp = A[i];
-			A[i] = min;
-			min = temp;
+    
+    for (int k = 0; k < size-1; k++){
+        int index = k;
+        for (int i = k+1; i < size; i++)
+            if ( ( num_cmps++, A[i] < A[index])) index = i;
+        int tmp = A[k]; // swap A[k] and A[index]
+        A[k] = A[index];
+        A[index] = tmp;
 
-		}
 	}
   /* Complete this function with the implementation of selection sort algorithm 
   Record number of comparisons in variable num_cmps of class Sort
